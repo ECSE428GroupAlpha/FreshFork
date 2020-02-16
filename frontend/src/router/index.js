c3c4bcbe3057194362ee4b0019b5a47cbb9f8e6c
ssmith <sean.smith3@mail.mcgill.ca>
2020-02-16 14:11:25 -0500

    UserProfile

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import UserProfile from "@/components/UserProfile"
 
 Vue.use(VueRouter);
 
@@ -27,6 +28,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Setup.vue")
+  },
+  {
+    path: '/userprofile',
+    name: 'UserProfile',
+    component: UserProfile
   }
 ];
 
