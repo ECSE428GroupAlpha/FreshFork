55fa7996dd75119f599ed435c3e7addd52ffe410
kim.young.q <43622703+kimyoungq17@users.noreply.github.com>
2020-02-17 13:46:24 -0500

    Create Login.vue
    added a login page

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <h1>Login</h1>
+    <hr>
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-3">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input
+            type="text"
+            class="form-control"
+            id="username"
+            v-model="username"
+            required
+            placeholder="Username"
+          >
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            v-model="password"
+            required
+            placeholder="Password"
+          >
+        </div>
+
+        <br>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    register: function() {
+      console.log("Username " + this.username);
+      console.log("password " + this.password);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
